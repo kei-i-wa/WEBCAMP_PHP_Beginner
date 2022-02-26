3483bafb82460ea3def14b5ada0065a6eca7ccc1
keiiwasaki <kei.kumarg@gmail.com>
2022-02-26 12:26:26 +0000

    WEBCAMP_Log.php log_use.php log_use_2.phpを書いた

---
+++ WEBCAMP_Log.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WEBCAMP;
+
+class Log{
+    public function test(){
+        echo "WEBCAMP内LOG内　test \n";
+    }
+}
---
+++ log_use.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once(__DIR__."WEBCAMP_Log.php");
+
+//　名前空間名、クラス名
+$obj = new \WEBCAMP\Log();
+$obj->test();
